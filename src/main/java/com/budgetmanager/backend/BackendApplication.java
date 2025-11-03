faee3c2f67971b16548a20e994ec4d893229103b
Akhil <chanduakhil777@gmail.com>
2025-11-03 15:08:11 +0530

    Fix: Exclude H2 from test dependencies

--- src/main/java/com/budgetmanager/backend/BackendApplication.java
+++ src/main/java/com/budgetmanager/backend/BackendApplication.java
@@ -2,12 +2,19 @@ package com.budgetmanager.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// --- 1. ADD THIS IMPORT ---
+import org.springframework.boot.autoconfigure.h2.H2ConsoleAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+// --- 2. MODIFY THIS LINE ---
+@SpringBootApplication(exclude = {H2ConsoleAutoConfiguration.class})
+// ---
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
 }
